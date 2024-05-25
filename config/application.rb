7ee3de47223f1103cfa6945f06299bede8db6db2
hatrang783 <03hanguyentrang@gmail.com>
2024-05-25 23:09:59 +0700

    config host update

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,6 @@ module NguyenDucAnh0123456789LibrarySystemFinalSubmission
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.hosts << "uniittechnologies.up.railway.app"
+    config.hosts << "https://web-production-e5e8.up.railway.app/"
   end
 end
